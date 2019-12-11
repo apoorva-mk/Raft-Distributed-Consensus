8004c1f499a8bba78684ae692433fdea484099b1
Neeraj <neeraj.deshpande3@gmail.com>
2019-12-11 13:43:38 +0530

    Made necessary changes

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -16,39 +16,32 @@ func TestMain(t *testing.T) {
 	if os.IsNotExist(err) {
 		t.Fatalf("Config file does not exist")
 	}
-	//check if there is no read permission to config file
-	mask := info.Mode()
-	if mask&(1<<2) == 0 {
-		t.Fatalf("No read permission to config file")
-	}
 	//check for Empty config file
 	if info.Size() == 0 {
 		t.Fatalf("Config file is empty")
 	}
-	//check for Invalid config file
+	//Check for opening of file
 	file, err := os.Open("../server.config.json")
+	if err != nil {
+		t.Fatalf("Error opening file")
+	}
 	decoder := json.NewDecoder(file)
+	//check for Invalid config file
 	configuration := make(map[int]types.Server)
 	err = decoder.Decode(&configuration)
 	if err != nil {
-		t.Errorf("Check if the config file is in the correct format")
 		t.Fatalf("Invalid config file")
 	}
-	//check for invalid IP or Port
+	//check for invalid IP and port
 	for _, v := range configuration {
 		IP := v.IP
 		_, err := regexp.MatchString(`^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$`, IP)
 		if err != nil {
 			t.Fatalf("Invalid IP Address in config file")
 		}
-		Port := v.Port
-		port, err := strconv.Atoi(Port)
-		if err != nil {
-			t.Fatalf("Invalid Port")
-		}
-		if port < 1024 {
-			t.Fatalf("Invalid Port number")
+		port := v.Port
+		if _, err := strconv.Atoi(port); err != nil {
+			t.Fatalf("Invalid Port Number in config file")
 		}
 	}
-	main()
 }
